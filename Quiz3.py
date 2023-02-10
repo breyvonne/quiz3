7974d3576ccdbee2b6deab6c8d04aa543bea7b22
Breanna <123026437+breyvonne@users.noreply.github.com>
2023-02-10 11:37:57 -0500

    Add files via upload

---
+++ Quiz3.py
@@ -0,0 +1,23 @@
+#Breanna Eafon and Grace Anspach
+def net_value(salary):
+    value=0.14*(salary-100)+0.24*(salary+100)
+    print(value)
+
+
+def next_stop(location):
+    print("next stop is",location)
+    print("next stop is",location)
+
+import math
+def circ(radius):
+    area=math.pi*(radius)**2
+    circum=2*math.pi*(radius)
+    print(area-circum)
+
+def name(yn):
+    print("Hello",yn,"!")
+
+def problem9(x):
+        y= math.tan(x) /(math.tan(x)+1)**2
+        value=math.sin(x)**3 +math.cos(x)-3*y
+        print(value)
